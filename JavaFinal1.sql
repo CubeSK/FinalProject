a661df71732379c4fcf46c3dbd6cb180223769d6
LauraHarmsena <122637980+LauraHarmsena@users.noreply.github.com>
2023-01-27 16:13:51 +0200

    Add files via upload

--- JavaFinal1.sql
+++ JavaFinal1.sql
@@ -1,1 +1,12 @@
-select * from javabooklistexperiment;
+select * from javabooks;
+alter table javabooks
+rename column ï»¿Author to Author;
+drop table javabooks;
+drop table javabooklistexperiment;
+insert into javabooks values ('xxx', 'yyy', 'zzz', 300, 2000, 2000,'History');
+create table Books
+as
+select
+*
+from javabooks;
+select * from books;
