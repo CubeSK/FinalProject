22ce0095b01c645e2be242a766e1d5bd5a1624e1
LauraHarmsena <122637980+LauraHarmsena@users.noreply.github.com>
2023-01-31 10:47:42 +0200

    Add files via upload

---
+++ JavaFinal1.sql
@@ -0,0 +1,64 @@
+CREATE TABLE Finalbooks 
+(
+    BookID int NOT NULL auto_increment primary key,
+    Author	VARCHAR(512) NOT NULL,
+    Region	VARCHAR(512) NOT NULL,
+    Title	VARCHAR(512) NOT NULL,
+    Pages	INT NOT NULL,
+    Published	INT NOT NULL,
+    OriginalYear	INT NOT NULL,
+    Genre	VARCHAR(512) NOT NULL
+);
+
+
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('David Howarth', 'Europe', 'The Sledge Patrol', '240', '2018', '2018', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Kelly Lytle Hernandez', 'South America', 'Bad Mexicans', '384', '2022', '2022', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Jon Meacham', 'North America', 'And There Was Light: Abraham Lincoln and the American Struggle', '720', '2022', '2022', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Anne Frank', 'Europe', 'Tales from the Secret Annex', 341, 1996, 1947, 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Viktor Suvorov', 'Asia', 'SpetsNorth Americaz: The Inside Story of the Soviet Special Forces', '207', '1988', '1988', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Miyamoto Musashi', 'Asia', 'The Book of Five Rings', '60', '2010', '2010', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Plato', 'Europe', 'The Trial and Death of Socrates', '58', '2000', '1892', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('John McPhee', 'North America', 'The Control of North Americature', '288', '1990', '1990', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Emilia Viotti da Costa', 'South America', 'The Brazilian Empire: Myths and Histories', '352', '2000', '2000', 'History');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Colleen Hoover', 'North America', 'Regretting You', '365', '2019', '2019', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Charlotte Bronte', 'Europe', 'Jane Eyre', '624', '2006', '1847', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Leo Tolstoy', 'Asia', 'AnNorth America KareniNorth America', '848', '1997', '1878', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Umberto Eco', 'Europe', 'The North Americame of the Rose', '472', '1994', '1980', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Diana Gabaldon', 'North America', 'Outlander: A Novel', '642', '2004', '2004', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Laura Esquivel', 'South America', 'Like Water for Chocolate', '256', '1995', '1995', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Mary Castillo', 'South America', 'Friday Night Chicas', '352', '2005', '2005', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Danielle Steel', 'North America', 'Without a Trace: A Novel', '217', '2023', '2023', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Yukio Mishima', 'Asia', 'The Sound of Waves', '192', '1994', '1994', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Marquis de Sade', 'Europe', 'Justine, or The Misfortunes of Virtue', '264', '2013', '1787', 'Romance');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Tedd Tripp', 'North America', 'Shepherding a Child\'s Heart', '215', '1995', '1995', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Alice Miller', 'Europe', 'The Drama of the Gifted Child', '133', '2008', '2008', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Jackie Mize', 'North America', 'SuperNorth Americatural Childbirth', '130', '2010', '1993', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Sufi Hart', 'Asia', 'How to be a Working Mum: marriage, children, childcare', '115', '2022', '2022', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Heidi Mavir', 'Europe', 'Your Child Is Not Broken', '156', '2022', '2022', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Rob Kemp', 'Europe', 'The Expectant Dad\'s Survival Guide', '272', '2010', '2010', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Gordon Neufeld', 'North America', 'Hold on to Your Kids: Why Parents Need to Matter More', '352', '2006', '2006', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Suzy Giordano', 'North America', 'Twelve Hours\' Sleep bt Twelve Weeks Old', '160', '2006', '2006', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Sally Shaywitz', 'North America', 'Overcoming Dyslexia', '414', '2020', '2005', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Eyal Ben-Ari', 'Asia', 'Japanese Childcare: An Interpretive Study of Culture and Organization', '250', '1997', '1997', 'Parenting');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Greta Thunberg', 'Europe', 'The Climate Book: Greta Thunberg', '464', '2022', '2022', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Ramona Coman', 'Europe', 'GoverNorth Americance and Politics in the Post-Crisis European Union', '444', '2020', '2020', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('George Orwell', 'Europe', '1984', '324', '2022', '1949', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Robin diAngelo', 'North America', 'White Fragility: Why It\'s So Hard for White People to Talk About Racism', '192', '2018', '2018', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Donella H. Meadows', 'North America', 'Thinking in Systems: InterNorth AmericatioNorth Americal Bestseller', '240', '2008', '2008', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Vasily Grossman', 'Asia', 'The Complete Black Book of Russian Jewry', '628', '2003', '2003', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Hwang Sok-yong', 'Asia', 'Gwangju Uprising: The Rebellion for Democracy in South Korea', '512', '2022', '2022', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Andrei Lankov', 'Asia', 'The Real North Korea: Life and Politics in the Failed Stalinist Utopia', '336', '2014', '2014', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Michael Beckley', 'North America', 'Danger Zone: The Coming Conflict with ChiNorth America', '304', '2022', '2022', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Volodymyr Zelensky', 'Europe', 'A MesSouth Americage from Ukraine: Speeches, 2019-2022', '144', '2022', '2022', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Orlando Gutierrez-Boronat', 'South America', 'Cuba: The Doctrine of The Lie', '198', '2022', '2022', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Santiago Levy', 'South America', 'Good Intentions, Bad Outcomes: Social Policy, Informality, and Economic Growth in Mexico', '372', '2008', '2008', 'Politics');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Jesie Ostermiller', 'Europe', 'The Colorwork Bible: Techniques and Projects for Colorful Knitting', '160', '2020', '2020', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Wade Bourne', 'North America', 'Basic Fishing: A Beginner\'s Guide', '224', '2015', '2015', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Pamelia Phillips', 'North America', 'Singing For Dummies: 3rd Edition', '384', '2021', '2021', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('James Paris', 'Europe', 'Growing Berries - How to Grow and Preserve Berries', '78', '2021', '2021', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Johanna Lundstrom', 'Europe', 'Sewing Activewear: How to Make Your Own ProfessioNorth Americal-looking athletic wear', '184', '2017', '2017', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Matsuo Basho', 'Asia', 'On Love and Barley: The Haiku of Basho', '92', '1985', '1985', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Chester Ong', 'Asia', 'Chinese Houses: The Architectural Heritage of a North Americation', '288', '2006', '2006', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Roberto Silva', 'South America', 'New Brazilian Gardens: The Legacy of Burle Marx', '192', '2006', '2006', 'Hobbies');
+INSERT INTO Finalbooks (Author, Region, Title, Pages, Published, OriginalYear, Genre) VALUES ('Nilda Callanaupa Alvarez', 'South America', 'Faces of Tradition: Weaving Elders of the Andes', '152', '2013', '2013', 'Hobbies');
+select * from Finalbooks;
